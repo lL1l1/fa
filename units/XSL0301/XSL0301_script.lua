384edd88daa7a7fb84cae358d6b78f16b73fce58
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-01-26 12:25:21 -0800

    Fix SACU range upgrade not using BP values for OC

--- units/XSL0301/XSL0301_script.lua
+++ units/XSL0301/XSL0301_script.lua
@@ -158,9 +158,9 @@ XSL0301 = ClassUnit(CommandUnit) {
             local wep = self:GetWeaponByLabel('LightChronatronCannon')
             wep:ChangeMaxRadius(bp.NewMaxRadius or 35)
             local wep = self:GetWeaponByLabel('OverCharge')
-            wep:ChangeMaxRadius(35)
+            wep:ChangeMaxRadius(bp.NewMaxRadius or 35)
             local aoc = self:GetWeaponByLabel('AutoOverCharge')
-            aoc:ChangeMaxRadius(35)
+            aoc:ChangeMaxRadius(bp.NewMaxRadius or 35)
         elseif enh == 'EnhancedSensorsRemove' then
             local bpIntel = self.Blueprint.Intel
             self:SetIntelRadius('Vision', bpIntel.VisionRadius or 26)
