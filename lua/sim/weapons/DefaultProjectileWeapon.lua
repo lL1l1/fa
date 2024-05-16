f9a8ffd9195374d954dd7f61a87fa12d83dd538f
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-05-16 05:25:15 -0700

    Fix MuzzleVelocityReduceDistance condition

--- lua/sim/weapons/DefaultProjectileWeapon.lua
+++ lua/sim/weapons/DefaultProjectileWeapon.lua
@@ -1150,7 +1150,7 @@ DefaultProjectileWeapon = ClassWeapon(Weapon) {
                 -- it's usually okay for a salvo to continue firing unless MuzzleVelocityReduceDistance is present, which requires a target to always exist
                 -- or else the projectile will fire at a very high speed, so we need to pack up/idle in that case
                 local bp = self.Blueprint
-                if bp.MuzzleVelocityReduceDistance then
+                if bp.MuzzleVelocityReduceDistance > 0 then
                     if bp.WeaponUnpacks then
                         ChangeState(self, self.WeaponPackingState)
                     else
