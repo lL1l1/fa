720f0ab450db4390dd22f99bb66094d72b460739
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-01 17:26:19 -0800

    Remove ipairs iterators from `TryRebuildUnits`
    Create fix.6609.md
    
    Remove logging
    
    debug logging
    
    Use with replay 23962051 to see drones not being cleaned up

--- lua/SimUtils.lua
+++ lua/SimUtils.lua
@@ -509,7 +509,7 @@ end
 ---@param army Army
 function TryRebuildUnits(trackers, army)
     local rebuilders = {}
-    for k, tracker in ipairs(trackers) do
+    for k, tracker in trackers do
         if tracker.Success then
             continue
         end
@@ -531,7 +531,7 @@ function TryRebuildUnits(trackers, army)
 
     WaitTicks(1)
 
-    for k, rebuilder in ipairs(rebuilders) do
+    for k, rebuilder in rebuilders do
         local tracker = trackers[k]
         local newUnit = rebuilder:GetFocusUnit()
         local progressDif = rebuilder:GetWorkProgress() - tracker.UnitProgress
